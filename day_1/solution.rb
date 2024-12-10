b1ecaa140e85c3cff8b35519761a146ac6a0e414
Nikhil Vengal <ngvengal@gmail.com>
2024-12-09 17:16:54 -0800

    Solve part 2

--- day_1/solution.rb
+++ day_1/solution.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+# part 1
+
 l1, l2 = [], []
 File.readlines('input.txt', chomp: true).each do |line|
   a, b = line.split(/ +/)
@@ -8,3 +10,12 @@ File.readlines('input.txt', chomp: true).each do |line|
 end
 
 puts l1.sort.zip(l2.sort).map { (_1[0] - _1[1]).abs }.sum
+
+# part 2
+
+freq = l2.each_with_object({}) do |loc, hits|
+  hits[loc] ||= 0
+  hits[loc] += 1
+end
+
+puts l1.map { (freq[_1] || 0) * _1 }.sum
