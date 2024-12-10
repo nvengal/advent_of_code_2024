b150fccb215559b1d0ee8c0879850acbec78711a
Nikhil Vengal <ngvengal@gmail.com>
2024-12-09 18:13:11 -0800

    Generate all plausible reports

--- day_2/solution.rb
+++ day_2/solution.rb
@@ -3,36 +3,25 @@
 # part 1
 
 class Report
-  attr_reader :report
+  attr_reader :report, :errors
 
   def initialize(report)
     @report = report
   end
 
   def valid?
-    _valid? || Report.new(report.reverse)._valid?
-  end
+    return true if report.size < 2
 
-  def _valid?
-    valid_pair?(*report[..1]) && rest_is_valid?(report[1..])
-  end
-
-  private
-
-  def valid_pair?(a, b)
-    diff = b - a
-    diff >= 1 && diff <= 3
-  end
-
-  def rest_is_valid?(rest)
-    return true if rest.count < 2
-
-    valid_pair?(*rest[..1]) && rest_is_valid?(rest[1..])
+    diff = report[1] - report[0]
+    diff >= 1 && diff <= 3 && Report.new(report[1..]).valid?
   end
 end
 
 reports = File.readlines('input.txt', chomp: true).map do |line|
-  Report.new line.split(/ +/).map(&:to_i)
-end
+  [
+    Report.new(line.split(/ +/).map(&:to_i)),
+    Report.new(line.split(/ +/).map(&:to_i).reverse)
+  ]
+end.flatten
 
-puts reports.filter { _1.valid? }.count
+puts reports.filter(&:valid?).count
