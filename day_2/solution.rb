77eb36bca9109edfe397d26fd1cf8b40bb3af152
Nikhil Vengal <ngvengal@gmail.com>
2024-12-09 22:15:28 -0800

    Allow elves to remove singular bad val

--- day_2/solution.rb
+++ day_2/solution.rb
@@ -3,25 +3,34 @@
 # part 1
 
 class Report
-  attr_reader :report, :errors
+  attr_reader :report, :dampenable, :start
 
-  def initialize(report)
+  def initialize(report, dampenable, start = [])
     @report = report
+    @dampenable = dampenable
+    @start = start
   end
 
   def valid?
     return true if report.size < 2
 
     diff = report[1] - report[0]
-    diff >= 1 && diff <= 3 && Report.new(report[1..]).valid?
+    if diff >= 1 && diff <= 3
+      Report.new(report[1..], dampenable, [*start, report[0]]).valid?
+    elsif dampenable
+      Report.new([*start, report[0], *report[2..]], false).valid? ||
+        Report.new([*start, report[1], *report[2..]], false).valid?
+    else
+      false
+    end
   end
 end
 
 reports = File.readlines('input.txt', chomp: true).map do |line|
   [
-    Report.new(line.split(/ +/).map(&:to_i)),
-    Report.new(line.split(/ +/).map(&:to_i).reverse)
+    Report.new(line.split(/ +/).map(&:to_i), true),
+    Report.new(line.split(/ +/).map(&:to_i).reverse, true)
   ]
-end.flatten
+end
 
-puts reports.filter(&:valid?).count
+puts reports.map { |a, b| a.valid? || b.valid? }.count { _1 }
