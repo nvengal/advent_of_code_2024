b29b6902c3563b1848fa9d93544322468c6e0829
Nikhil Vengal <ngvengal@gmail.com>
2024-12-10 12:46:16 -0800

    Solve pt 2

--- day_3/solution.rb
+++ day_3/solution.rb
@@ -1,7 +1,9 @@
 #!/usr/bin/env ruby
 
-captures = File.readlines("input.txt", chomp: true).map do |line|
-  line.scan(/(mul\(\d+,\d+\))/).flatten
-end.flatten
+captures = File.readlines("input.txt", chomp: true).join
+  .split(/do\(\)/)
+  .map { _1.split(/don't\(\)/) }
+  .map { _1[0].scan(/(mul\(\d+,\d+\))/).flatten }
+  .flatten
 
 puts captures.map { _1.scan(/(\d+),(\d+)/).first.map(&:to_i).reduce(&:*) }.sum
