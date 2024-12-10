21b561a4f6c80b1851b91b5b0c00366382f8399f
Nikhil Vengal <ngvengal@gmail.com>
2024-12-10 12:30:32 -0800

    Solve pt 1

--- day_3/solution.rb
+++ day_3/solution.rb
@@ -1,1 +1,7 @@
 #!/usr/bin/env ruby
+
+captures = File.readlines("input.txt", chomp: true).map do |line|
+  line.scan(/(mul\(\d+,\d+\))/).flatten
+end.flatten
+
+puts captures.map { _1.scan(/(\d+),(\d+)/).first.map(&:to_i).reduce(&:*) }.sum
