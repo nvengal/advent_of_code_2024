6bcb5e7de89e480c96656e257c2dae152a812b12
Nikhil Vengal <ngvengal@gmail.com>
2024-12-11 16:23:42 -0800

    Dynamic programming for the win

--- day_11/solution.rb
+++ day_11/solution.rb
@@ -1,20 +1,24 @@
 #!/usr/bin/env ruby
 
-stones = File.readlines("input.txt", chomp: true)[0].split(" ")
+@memo = {}
 
-25.times do
-  stones = stones.map do |stone|
-    next '1' if stone == '0'
+def blink(val, num)
+  @memo[val] ||= []
+  return 1 if num == 0
+  return @memo[val][num] if @memo[val][num]
 
-    if stone.length.even?
-      next [
-        stone[...(stone.length/2)],
-        stone[(stone.length/2)..].to_i.to_s
-      ]
-    end
-
-    (stone.to_i * 2024).to_s
-  end.flatten
+  if val == '0'
+    @memo[val][num] = blink('1', num - 1)
+  elsif val.length.even?
+    @memo[val][num] =
+      blink(val[...(val.length/2)], num - 1) +
+      blink(val[(val.length/2)..].to_i.to_s, num - 1)
+  else
+    @memo[val][num] = blink((val.to_i * 2024).to_s, num - 1)
+  end
 end
 
-puts stones.count
+stones = File.readlines("input.txt", chomp: true)[0].split(" ")
+
+puts "25: " + stones.map { blink(_1, 25) }.sum.to_s
+puts "75: " + stones.map { blink(_1, 75) }.sum.to_s
